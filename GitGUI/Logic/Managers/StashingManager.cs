080b7c90c18bf2c96a507eb48dc1f98151017df4
JaroslavVecera <44584182+JaroslavVecera@users.noreply.github.com>
2020-10-07 20:28:31 +0200

    StashingManager

---
+++ GitGUI/Logic/Managers/StashingManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+
+namespace GitGUI.Logic
+{
+    class StashingManager
+    {
+        StashCollection _stashes;
+        string _dirPath;
+
+        public void SetRepository(Repository r)
+        {
+            _stashes = r.Stashes;
+            _dirPath = r.Info.Path;
+        }
+
+        public void ImplicitPush(CommitNodeModel model)
+        {
+
+        }
+
+        public void ImplicitPop(CommitNodeModel model)
+        {
+
+        }
+
+        public void Pop(CommitNodeModel model)
+        {
+
+        }
+
+        public void PopLast()
+        {
+            _stashing.Pop(0);
+        }
+
+        public void Push(CommitNodeModel model)
+        {
+
+        }
+
+        int FindIndex(CommitNodeModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
